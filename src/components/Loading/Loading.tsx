5e7418f056ecb29ae26d9eb163889e4c2b38c100
unknown <rmtinrostmi@gmail.com>
2024-06-04 12:13:32 +0430

    checkin

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -7,7 +7,7 @@
 // import {ReactComponent as Img2} from '../Loading/svg/MainLogoOKR.svg';
 // import {ReactComponent as Img3} from '../Loading/svg/Group 4242492.svg';
 // import {ReactComponent as OKRText} from '../Loading/svg/OKRText.svg';
-// import {ReactComponent as Omid} from '../Loading/svg/trace.svg';
+
 // import mainLgo from '../Loading/svg/logo192.png'
 
 
@@ -20,17 +20,21 @@
 //         backgroundImage:`url(${BackGround})`,
 //         backgroundSize:'cover'
 //     }}   >
-// <Omid style={{width:'500px', height:'400px'}}/>
-// <Stack spacing={2} alignItems='center'  justifyContent='center'  sx={{width:'150px',height:'150px'}} mx="auto" >
-// <Box width='150px' >
-// <Box  mx={'auto'}  mt={8}  display={'flex'} justifyContent={'center'}>
+// <Stack spacing={2} alignItems='center'  justifyContent='center'  sx={{width:'800px',height:'100%'}} mx="auto" >
 
+// <Box width='100%' >
+// <Box  mx={'auto'}  mt={10}  display={'flex'} justifyContent={'center'}>
 // <Img2 onClick={()=>{
 // // confirm('test')
-// }} style={{width:'200px'}} />
+// }} style={{width:'400px'}} />
 // </Box>
-// <OKRText style={{width:'100px', height:'100px'}}/>
 // </Box>
+// <Box>
+//   <OKRText/>
+// </Box>
+// {/* <Box>
+// <Img1/>
+// </Box> */}
 
 // <Box sx={{width:'150px',textAlign:'center'}} mt={2} >
 // {
@@ -61,56 +65,49 @@ import Progress from '../Progress/Progress';
 import BackGround from '../Login/Statics/Imgs/bg3.png'
 
 import {ReactComponent as Img1} from '../Loading/svg/Group 4242492.svg';
-import {ReactComponent as Img2} from '../Loading/svg/MainLogoOKR.svg';
+import {ReactComponent as Img2} from '../Loading/svg/MainLogoOKRR.svg';
 import {ReactComponent as Img3} from '../Loading/svg/Group 4242492.svg';
-import {ReactComponent as OKRText} from '../Loading/svg/OKRText.svg';
-
+import {ReactComponent as OKR} from '../Loading/svg/OKR.svg';
+import {ReactComponent as Omid} from '../Loading/svg/Omid.svg';
 import mainLgo from '../Loading/svg/logo192.png'
 
 
 const Loading = ({setContentState,loading}:any) => {
   return (
-    <Grid container sx={{
+    <Grid overflow={'hidden'} container sx={{
      
-       minHeight:'100vh',
+        minHeight:'100vh',
         overflowY:'auto',
         backgroundImage:`url(${BackGround})`,
         backgroundSize:'cover'
     }}   >
-<Stack spacing={2} alignItems='center'  justifyContent='center'  sx={{width:'800px',height:'100%'}} mx="auto" >
+<Stack  alignItems='center'  justifyContent='center'  sx={{width:'555px',height:'100%'}}>
 
-<Box width='100%' >
-<Box  mx={'auto'}  mt={10}  display={'flex'} justifyContent={'center'}>
-<Img2 onClick={()=>{
-// confirm('test')
-}} style={{width:'400px'}} />
-</Box>
-</Box>
-<Box>
-  <OKRText/>
-</Box>
 {/* <Box>
 <Img1/>
+
 </Box> */}
 
-<Box sx={{width:'150px',textAlign:'center'}} mt={2} >
+{/* <Box sx={{width:'150px',textAlign:'center'}} mt={2} >
 {
 loading && <Progress setContentState={setContentState} />
 }
 
+</Box> */}
+<Box  display={'flex'} margin={5} flexDirection={'column'} justifyContent={'center'} marginRight={2} >
+  <Omid/>
+  </Box>
+
+<Box display={'flex'} alignItems={'center'} marginTop={10} flexDirection={'column'}  justifyContent={'center'}>
+ <a target='-blank' href='https://okrcoach.ir/'> <Img2/></a>
+ <a target='-blank' href='https://okrcoach.ir/'> <OKR/></a>
 </Box>
 
 
-
-{/* <Box width='100%'  >
-   <Box width='100%' display='flex' flexDirection='row-reverse' >
-   <Img3 style={{width:'150px',height:'150px'}}/>
-   </Box>
-</Box> */}
-</Stack>
-        
+</Stack>     
 
     </Grid>
+    
   )
 }
 
